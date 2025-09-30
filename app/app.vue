dba76acbbf11d497159590baa2c3b8f399b666c0
Viktor <iinegnalon@gmail.com>
2025-09-30 13:55:35 +0400

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <Head>
+      <Title>Airesto</Title>
+      <Meta content="Airesto" name="description" />
+      <Meta content="width=device-width, initial-scale=1" name="viewport" />
+    </Head>
+    <Body>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </div>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div class="main-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
